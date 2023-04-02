151b8caa1d954d2fe70ce715935db289e0cd7bb0
macafsn16 <103944518+macafsn16@users.noreply.github.com>
2023-04-02 17:27:45 -0300

    acttualizacion

---
+++ my-custom-app/src/pages/componente/button.js
@@ -0,0 +1,14 @@
+export default function Button() {
+    /* Si esto NO fuera JSX
+    return React.createElement(
+      'button',
+      {  },
+      'El Boton de ReactJS'
+    );*/
+
+    /* Esto es jsx */ 
+    return (
+      <button>El Boton desde React con JSx</button>
+    );
+  
+}
