f7e1d073c8a5240e99f0e039304203c06e0326d1
macafsn16 <103944518+macafsn16@users.noreply.github.com>
2023-04-02 19:59:03 -0300

    Actualizar casi listo

--- my-custom-app/src/pages/componente/card.js
+++ my-custom-app/src/pages/componente/card.js
@@ -1,7 +1,12 @@
-export default function Card( { data } ) {
+export default function Card( {data} ) {
     return(
-        <span>
-            { data.text }
-        </span>
+        <div className="Presentacion">
+            <title>{ data.titulo}</title>
+            <h1>{data.instruccion} </h1>
+            <img src={data.imagen} />
+            <span>
+                { data.text }
+            </span>
+        </div>
         );
 }
