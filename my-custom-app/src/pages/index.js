fa2f997fda6051f43614761d68fc8d104f4234f3
macafsn16 <103944518+macafsn16@users.noreply.github.com>
2023-04-02 17:46:18 -0300

    act42

--- my-custom-app/src/pages/index.js
+++ my-custom-app/src/pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Button from './componentes/button';
-import Card from './componentes/card';
+import Button from './componentes/button'
+import Card from './componentes/card'
 
 const inter = Inter({ subsets: ['latin'] })
 
