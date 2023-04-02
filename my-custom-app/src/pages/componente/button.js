bcd8f057f7021687aa0a91ec33420764be7f7040
macafsn16 <103944518+macafsn16@users.noreply.github.com>
2023-04-02 17:45:58 -0300

    act4

--- my-custom-app/src/pages/componente/button.js
+++ my-custom-app/src/pages/componente/button.js
@@ -1,14 +1,7 @@
 export default function Button() {
-    /* Si esto NO fuera JSX
-    return React.createElement(
-      'button',
-      {  },
-      'El Boton de ReactJS'
-    );*/
 
-    /* Esto es jsx */ 
     return (
-      <button>El Boton desde React con JSx</button>
+      <button>Venezuela</button>
     );
   
 }
